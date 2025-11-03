6f7a985dde040f85d7750aaa1cc03944121844b4
Manii-07 <usmanakramkumbher18jan@gmail.com>
2025-11-03 23:43:54 +0500

    Add files via upload

---
+++ LAB10_COAL_USMAN AKRAM_65587/L10_T4_65587.asm
@@ -0,0 +1,24 @@
+org 100h
+.data
+a db "USMAN AKRAM"
+.code
+mov ax,@data
+mov ds,ax
+mov si,offset a
+mov cx,10
+l1:
+mov bx,[si]
+push bx
+inc si
+loop l1
+mov cx,10
+l2:
+pop dx 
+mov ah,2 
+mov dl,dl 
+int 21h
+loop l2
+
+
+
+
---
+++ LAB10_COAL_USMAN AKRAM_65587/L10_T5_65587.asm
@@ -0,0 +1,19 @@
+org 100h
+.data
+a db "USMAN AKRAM"
+.code
+mov ax, @data
+mov ds, ax
+mov si, offset a
+mov cx, 10 
+
+l1:
+mov dl, [si]
+inc si 
+mov ah,2
+int 21h
+        
+loop l1    
+
+
+
